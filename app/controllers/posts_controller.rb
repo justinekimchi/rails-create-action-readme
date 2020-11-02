52ab2c8624e4fbb8b711a5567b1d5f2c644dd9e6
justinekimchi <justine.kim.manaloto@gmail.com>
2020-11-02 14:48:32 -0600

    commit tree 30c233a12b6bf4838e41700de3cd300fd8aa41bd

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,28 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-  end
+    def index
+        @posts = Post.all
+    end
 
-  def show
-    @post = Post.find(params[:id])
-  end
+    def show
+        @post = Post.find(params[:id])
+    end
 
-  def new
-    @post = Post.new
-  end
+    def new
+        @post = Post.new
+    end
 
-  # add create method here
+    def create
+        @post = Post.new
+        @post.title = params[:title]
+        @post.description = params[:description]
+        @post.save
+        redirect_to post_path(@post)
+
+        # @post =
+        #     Post.create(
+        #         title: params[:post][:title],
+        #         description: params[:post][:description]
+        #     )
+        # redirect_to post_path
+    end
 end
